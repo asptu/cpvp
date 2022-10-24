1aa35aa235523d3806e9a926a1af725871789435
asptu <84566762+asptu@users.noreply.github.com>
2022-10-24 15:19:55 +1100

    front page test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,68 @@
   import { base } from "$app/paths";
 </script>
 
-<h1>Home</h1>
-<p>works !</p>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
-<a href="{base}/about">About</a>
+
+<div class='container'>
+  <div class='title-l1'>CRYSTAL PVP</div>
+  <div class='title-l2'>explained like a</div>
+  <div class='title-l3'>Fighting Game</div>
+</div>
+
+<video class='bg' width="1280" height="720" muted="muted" loop="loop" autoplay="autoplay">
+  <source type="video/mp4" src="/cpvp/footage/h264.mp4">
+</video>
+
+
+<style>
+
+.bg {
+  position: fixed;
+    width: 100%;
+    height: 108.38vh;
+    left: 0;
+    top: 0;
+    z-index: -1;
+    filter: brightness(20%);
+}
+
+
+.title-l1 {
+  font-size: 900%;
+  color: #ea5180;
+  padding-left: 25%;
+  padding-top: 15%;
+  font-family: 'THICCBOI-ExtraBold';
+}
+
+.title-l2 {
+  font-size: 500%;
+  color: #ffffff;
+  padding-left: 35%;
+  font-family: 'THICCBOI-Thin';
+}
+
+.title-l3 {
+  font-size: 1200%;
+  color: #e03333;
+  padding-left: 45%;
+  font-family: 'Makcex';
+}
+
+@font-face {
+  font-family: 'THICCBOI-ExtraBold';
+  src: URL('src/assets/fonts/THICCCBOI-ExtraBold.ttf') format('truetype');
+}
+
+@font-face {
+  font-family: 'THICCBOI-Thin';
+  src: URL('src/assets/fonts/THICCCBOI-Thin.ttf') format('truetype');
+}
+
+@font-face {
+  font-family: 'Makcex';
+  src: URL('src/assets/fonts/Makcex.otf') format('truetype');
+}
+
+
+
+</style>
--- src/routes/test1/+page.svelte
+++ src/routes/test1/+page.svelte
@@ -1,1 +1,37 @@
-<h1> Test1 page </h1>
+<script>
+	
+	function handleAnchorClick (event) {
+		event.preventDefault()
+		const link = event.currentTarget
+		const anchorId = new URL(link.href).hash.replace('#', '')
+		const anchor = document.getElementById(anchorId)
+		window.scrollTo({
+			top: anchor.offsetTop,
+			behavior: 'smooth'
+		})
+	}
+
+</script>
+
+
+<a class= 'balls' href="#facebook_ads_example">
+	balls
+</a>
+
+<p>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br></p>
+
+
+<h2 class = 'balls'id="facebook_ads_example">real</h2>
+<p>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br>t<br></p>
+
+<style>
+
+    p {
+        font-size: 100px;
+    }
+
+    .balls {
+        scroll-behavior: smooth;
+        transition: all .4s ease;
+    }
+</style>
